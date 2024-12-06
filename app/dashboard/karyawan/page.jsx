721ff7e757205c0cb7a6d8be6f825ea805bddea9
rifkidocs <rifkiars@gmail.com>
2024-12-06 09:14:04 +0700

    ui: improve responsive in karyawan page

--- app/dashboard/karyawan/page.jsx
+++ app/dashboard/karyawan/page.jsx
@@ -8,7 +8,7 @@ export default function EmployeeManagement() {
     <div className='container mx-auto'>
       <h1 className='text-2xl font-bold mb-4'>Pengelolaan Karyawan</h1>
       <ExpiringContractsNotification />
-      <div className='mb-4 flex justify-between items-center'>
+      <div className='mb-4 flex justify-between md:items-center md:flex-row flex-col gap-y-2'>
         <AddEmployeeButton />
         <ExcelImportSimulation />
       </div>
--- components/dashboard-shell.jsx
+++ components/dashboard-shell.jsx
@@ -1,6 +1,6 @@
 export function DashboardShell({ children }) {
   return (
-    <div className='flex-1 space-y-4 p-8 pt-6 overflow-auto'>
+    <div className='flex-1 space-y-4 md:p-8 md:pt-6 p-4 overflow-auto'>
       <div className='grid gap-4'>{children}</div>
     </div>
   );
